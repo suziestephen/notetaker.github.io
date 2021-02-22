83ae44e70a05e8d1ff5ee3b9133d64184d1d8f70
suzietstephen32 <suziestephen@192-168-1-100.tpgi.com.au>
2021-02-22 22:00:22 +1100

    changes to variables

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 const path = require("path");
 
 const noteText = JSON.parse(
-  fs.readFileSync(path.join(__dirname, "../db/db.json"), (err, data) => {
+  fs.readFileSync(path.join(__dirname, "../db/db.json"), (err, text) => {
     if (err) throw err;
   })
 );
@@ -17,7 +17,6 @@ function writenoteText(noteText) {
     }
   );
 }
-
 
 module.exports = app => {
   app.get("/api/notes", (req, res) => res.json(noteText));
