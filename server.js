38205e12943bf401bdfc7b615a4545a7d33a67da
suzietstephen32 <suziestephen@192-168-1-100.tpgi.com.au>
2021-02-26 13:22:57 +1100

    fixing paths

--- server.js
+++ server.js
@@ -8,7 +8,8 @@ const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname,"../develop/public")));
+
+app.use(express.static("public"));
 
 
 require("./routes/apiroutes")(app);
