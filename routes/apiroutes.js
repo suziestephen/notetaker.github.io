f3bb84489338d517a647994a2777d17ebc95aa13
suzietstephen32 <suziestephen@MacBook-Pro-3.local>
2021-03-01 21:12:15 +1100

    removing changed code to fix new error

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -18,8 +18,6 @@ module.exports = app => {
         });
     }
 
-    app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
-    app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html')));
 
     app.get("/", function(req, res){
         res.json(notesData);
