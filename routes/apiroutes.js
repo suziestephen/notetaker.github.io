84535364c21cb8157b1d03c655aeeca684dd32d4
suzietstephen32 <suziestephen@MacBook-Pro-3.local>
2021-03-01 20:11:27 +1100

    changes to json code

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,46 +1,38 @@
 
 const fs = require("fs");
-const path = require("path");
-
-const noteText = JSON.parse(
-  fs.readFileSync(path.join(__dirname, "../db/db.json"), (err, text) => {
-    if (err) throw err;
-  })
-);
-
-function writenoteText(noteText) {
-  fs.writeFileSync(
-    path.join(__dirname, "../db/db.json"),
-    JSON.stringify(noteText),
-    err => {
-      if (err) throw err;
-    }
-  );
-}
+const path = require("path")
+const notesData = require(path.join(__dirname,"../db/db.json"));
 
 module.exports = app => {
-  app.get("/api/notes", (req, res) => res.json(noteText));
 
+function writenoteText(notes){
 
+  notes = JSON.stringify(notes);
+  console.log (notes);
 
-  app.post('/api/notes', (req,res) => {
-    let newNote = req.body; 
-    console.log("newNote: " + newNote);
-    console.log("data: ", data);
-    data.push(newNote);
-    console.log("new note pushed");
-      fs.writeFileSync(
-        path.join(__dirname,"../db/db.json"),
-        JSON.stringify(data),
-        console.log("data passed through JSON.stringify"),
-        err => {
-        if (err) throw err;
-        }
-      );
-
-      return res.json(data);
+  fs.writeFileSync(path.join(__dirname,"../db/db.json"), notes, function(err){
+      if (err) {
+          return console.log(err);
+      }
   });
-  
+}
+
+
+  app.get("/api/notes", (req, res) => 
+    res.json(notesData));
+
+  app.post("/api/notes", (req, res) =>
+    noteText.push(req.body));
+
+    writenoteText(notesData);
+    console.log(notesData);
+
+    res.json(req.body);
 };
+
+
+
+
+
 
 
