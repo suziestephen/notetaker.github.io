bfddeb4614cf6a22e16fb6f08eb570869f438b1b
suzietstephen32 <suziestephen@MacBook-Pro-3.local>
2021-03-01 20:21:00 +1100

    changing post code getting error issues

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,38 +1,32 @@
-
 const fs = require("fs");
-const path = require("path")
-const notesData = require(path.join(__dirname,"../db/db.json"));
+const notesData = require("../db/db.json");
+
 
 module.exports = app => {
 
-function writenoteText(notes){
-
-  notes = JSON.stringify(notes);
-  console.log (notes);
-
-  fs.writeFileSync(path.join(__dirname,"../db/db.json"), notes, function(err){
-      if (err) {
-          return console.log(err);
-      }
-  });
-}
 
 
-  app.get("/api/notes", (req, res) => 
-    res.json(notesData));
+    function writeNoteText(notes){
 
-  app.post("/api/notes", (req, res) =>
-    noteText.push(req.body));
+        notes = JSON.stringify(notes);
+        console.log (notes);
 
-    writenoteText(notesData);
-    console.log(notesData);
+        fs.writeFileSync("../db/db.json", notes, function(err){
+            if (err) {
+                return console.log(err);
+            }
+        });
+    }
 
-    res.json(req.body);
-};
+    app.get("/api/notes", function(req, res){
+        res.json(notesData);
+    });
 
+    app.post("/api/notes", function(req, res){
 
+        writeNoteText(notesData);
+        console.log(notesData);
 
-
-
-
-
+        res.json(req.body);
+    });
+  };
