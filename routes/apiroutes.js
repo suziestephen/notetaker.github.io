79393b7d02cabff8f977f241f9f49d756ea4e459
suzietstephen32 <suziestephen@MacBook-Pro-3.local>
2021-03-01 20:58:14 +1100

    adding in html file paths

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -18,7 +18,10 @@ module.exports = app => {
         });
     }
 
-    app.get("/api/notes", function(req, res){
+    app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
+    app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'public/notes.html')));
+
+    app.get("/", function(req, res){
         res.json(notesData);
     });
 
