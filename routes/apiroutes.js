013d931e0066496faf33db30b6fb0ee0998ff28c
suzietstephen32 <suziestephen@MacBook-Pro-3.local>
2021-03-01 21:23:59 +1100

    changes to test again

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -19,6 +19,9 @@ module.exports = app => {
     }
 
 
+    app.get('/', (req, res) => res.sendFile(path.join(__dirname, '../public/index.html')));
+    app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '../public/notes.html')));
+
     app.get("/api/notes", (req, res) => {
         res.json(notesData);
     });
