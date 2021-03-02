368cf3cc91524ccf820c7f5129a83cc354f2c715
suzietstephen32 <suziestephen@MacBook-Pro-4.local>
2021-03-02 20:39:12 +1100

    changing syntax

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,38 +1,38 @@
 const fs = require("fs");
 const path = require("path");
 
-const noteText = JSON.parse(
-  fs.readFileSync(path.join(__dirname, "../db/db.json"), (err, text) => {
+const data = JSON.parse(
+  fs.readFileSync(path.join(__dirname, "../db/db.json"), (err) => {
     if (err) throw err;
   })
 );
 
-console.log("data: ", noteText);
+console.log("data: ", data);
 
 
 
 module.exports = app => {
-  app.get("/api/notes", (req, res) => res.json(noteText));
+  app.get("/api/notes", (req, res) => res.json(data));
 
 
   app.post("/api/notes", (req, res) => {
     let newNote = req.body;
     console.log("newNote: " + newNote);
-    console.log("data: ", noteText);
+    console.log("data: ", data);
     data.push(newNote);
     console.log("NEW NOTE");
         fs.writeFileSync(
             path.join(__dirname, "../db/db.json"),
-            JSON.stringify(noteText),
+            JSON.stringify(data),
             err => {
             if (err) throw err;
             }
         );
-        return res.json(noteText);
+        return res.json(data);
         });
 
         app.get("api/notes/:id", function(req,res) {
-            res.json(noteText[req.params.id]);
+            res.json(data[req.params.id]);
             console.log("item selected")
            });
   
