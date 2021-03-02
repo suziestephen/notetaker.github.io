bd853a007b95584b5b964d4d71dead8205b9e8e4
suzietstephen32 <suziestephen@MacBook-Pro-4.local>
2021-03-02 19:44:51 +1100

    fixing paths

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,34 +1,31 @@
 const fs = require("fs");
-const notesData = require("../db/db.json");
+const path = require("path");
 
+const noteText = JSON.parse(
+  fs.readFileSync(path.join(__dirname, "../db/db.json"), (err, text) => {
+    if (err) throw err;
+  })
+);
+
+function writenoteText(noteText) {
+  fs.writeFileSync(
+    path.join(__dirname, "../db/db.json"),
+    JSON.stringify(noteText),
+    err => {
+      if (err) throw err;
+    }
+  );
+}
 
 module.exports = app => {
+  app.get("/api/notes", (req, res) => res.json(noteText));
 
-
-
-    function writeNoteText(notes){
-
-        notes = JSON.stringify(notes);
-        console.log (notes);
-
-        fs.writeFileSync("../db/db.json", notes, function(err){
-            if (err) {
-                return console.log(err);
-            }
-        });
-    }
-
-
-
-    app.get("/api/notes", (req, res) => {
-        res.json(notesData);
-    });
-
-    app.post("/api/notes", (req, res) => {
-
-        writeNoteText(notesData);
-        console.log(notesData);
-
-        res.json(req.body);
-    });
-  };
+  app.post("/api/notes", (req, res) => {
+    let data = req.body;
+    data.id = getId(noteText);
+    noteText.push(data);
+    writenoteText(noteText);
+    return res.json(noteText);
+  });
+  
+};
